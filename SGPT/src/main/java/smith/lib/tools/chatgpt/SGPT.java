ddab518a64335fe61c18d136f4d59eb7bab60e49
@smith8h <husseinshakir81@gmail.com>
2023-01-21 02:01:54 +0300

    Project initialized

---
+++ SGPT/src/main/java/smith/lib/tools/chatgpt/SGPT.java
@@ -0,0 +1,64 @@
+package smith.lib.tools.chatgpt;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.MediaType;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.RequestBody;
+import okhttp3.Response;
+import org.json.JSONObject;
+import java.io.IOException;
+
+public class SGPT {
+    
+    // this token is for test use only!
+    // please don't use it outside this library!
+    public static final String TEST_MODE = "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";
+	private String apiKey;
+    
+	public SGPT(String apiKey) {
+	    this.apiKey = apiKey;
+    }
+	
+	public void chat(String text, ChatCallBack chatcallback) {
+		JSONObject params = new JSONObject();
+		try{
+			params.put("prompt", text);
+			params.put("max_tokens", 4000);
+			params.put("temperature", 0.95);
+		}catch(Exception e){}
+		
+		Request req = new Request.Builder()
+		.url("https://api.openai.com/v1/engines/text-davinci-003/completions")
+		.header("Authorization", "Bearer " + apiKey)
+		.header("Content-Type", "application/json")
+		.post(RequestBody.create(MediaType.parse("application/json"), params.toString()))
+		.build();
+		
+        OkHttpClient client = new OkHttpClient();
+		client.newCall(req).enqueue(new Callback() {
+            @Override public void onResponse(Call call, Response response) throws IOException {
+			    String responseBody = response.body().string();
+                chatcallback.onResponse(parseResponse(responseBody));
+            }
+            
+            @Override public void onFailure(Call call, IOException e) {
+		    	chatcallback.onFailure("Something went wrong!");
+	    	}
+        });
+	}
+	
+	private String parseResponse(String responseBody){
+		String text = responseBody;
+		try {
+			text = ((new JSONObject(responseBody)).getJSONArray("choices")).getJSONObject(0).getString("text");
+		} catch (Exception e) {}
+		return text.replaceAll("(?m)^[ \t]*\r?\n","");
+	}
+    
+    public interface ChatCallBack {
+        public void onResponse(String response);
+        public void onFailure(String message);
+    }
+}
---
+++ app/src/main/java/smith/test/MainActivity.java
@@ -0,0 +1,35 @@
+package smith.test; 
+
+import android.os.Bundle; 
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+import smith.lib.tools.chatgpt.SGPT;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+    
+    public void chat(View v) {
+        EditText et = findViewById(R.id.et);
+        
+        SGPT chat = new SGPT(SGPT.TEST_MODE);
+        chat.chat(et.getText().toString(), new SGPT.ChatCallBack() {
+            @Override public void onResponse(String response) {
+                TextView tv = findViewById(R.id.tv);
+                tv.setText(response);
+            }
+            
+            @Override public void onFailure(String message) {
+                TextView tv = findViewById(R.id.tv);
+                tv.setText(message);
+            }
+        });
+    }
+}
